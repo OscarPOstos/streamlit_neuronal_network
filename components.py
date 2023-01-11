57cd6d3ff4bb54e22f89cb6cecb54c8a717678c9
OscarPOstos <110192091+OscarPOstos@users.noreply.github.com>
2023-01-11 08:19:56 +0100

    update components.py

--- components.py
+++ components.py
@@ -10,21 +10,28 @@ def show_components_tab1():
         st.text(neuronal_net(weight, x))
 
 def show_components_tab2():
-    weight = st.slider("Peso w0", 0.0, 5.0, key="wt21")
-    x = st.number_input("Entrada x0", step=1.0, key="xt21")
-    weight2 = st.slider("Peso w1", 0.0, 5.0, key="wt22")
-    x2 = st.number_input("Entrada x1", step=1.0, key="xt22")
+    col1, col2 = st.columns(2)
+    with col1:
+        weight = st.slider("Peso w0", 0.0, 5.0, key="wt21")
+        x = st.number_input("Entrada x0", step=1.0, key="xt21")
+    with col2:
+        weight2 = st.slider("Peso w1", 0.0, 5.0, key="wt22")
+        x2 = st.number_input("Entrada x1", step=1.0, key="xt22")
     submit2 = st.button("Calcular la salida", key="submit2")
     if submit2:
         st.text(neuronal_net_2(weight, weight2, x, x2))
 
 def show_components_tab3():
-    weight = st.slider("Peso w0", 0.0, 5.0, key="wt31")
-    x = st.number_input("Entrada x0", step=1.0, key="xt31")
-    weight2 = st.slider("Peso w1", 0.0, 5.0, key="wt32")
-    x2 = st.number_input("Entrada x1", step=1.0, key="xt32")
-    weight3 = st.slider("Peso w3", 0.0, 5.0, key="wt33")
-    x3 = st.number_input("Entrada x2", step=1.0, key="xt33")
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        weight = st.slider("Peso w0", 0.0, 5.0, key="wt31")
+        x = st.number_input("Entrada x0", step=1.0, key="xt31")
+    with col2:
+        weight2 = st.slider("Peso w1", 0.0, 5.0, key="wt32")
+        x2 = st.number_input("Entrada x1", step=1.0, key="xt32")
+    with col3:
+        weight3 = st.slider("Peso w2", 0.0, 5.0, key="wt33")
+        x3 = st.number_input("Entrada x2", step=1.0, key="xt33")
     bias = st.number_input("Introduzca un valor de sesgo", step=1.0)
     submit3 = st.button("Calcular la salida", key="submit3")
     if submit3:
